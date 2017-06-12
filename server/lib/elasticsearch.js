9a257176806dae57671605186995fadc69d03642
Mark MacGillivray <mark@cottagelabs.com>
2017-06-12 22:18:32 +0100

    altering logging

--- server/lib/elasticsearch.js
+++ server/lib/elasticsearch.js
@@ -10,8 +10,8 @@ if (!Meteor.settings || !Meteor.settings.es) {
   try {
     var s = Meteor.http.call('GET',Meteor.settings.es.url);
   } catch(err) {
-    API.log({msg:'ELASTICSEARCH INSTANCE AT ' + Meteor.settings.es.url + 'APPEARS TO BE UNREACHABLE. SHUTTING DOWN.', error:err});
-    process.exit(-1);
+    API.log({msg:'ELASTICSEARCH INSTANCE AT ' + Meteor.settings.es.url + ' APPEARS TO BE UNREACHABLE. SHUTTING DOWN.', error:err});
+    exit();
   }
 }
 
